0af0705096dccbc5126d4ffa6ca2dea9dfcd2ebf
psjishnu <psjishnu13@gmail.com>
2021-07-23 11:30:30 +0530

    fixes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,305 +1,38 @@
 import React, { useState } from "react";
-import axios from "axios";
-//import moment from "moment";
-import { Card, TextField } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-const baseURL = { url: process.env.REACT_APP_BASE_URL };
-const useStyles = makeStyles({
-  textField: {
-    width: "100px",
-  },
-  text1: {
-    width: "100px",
-    backgroundColor: "red",
-    color: "white",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  text2: {
-    width: "100px",
-    backgroundColor: "green",
-    color: "white",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  text3: {
-    width: "100px",
-    backgroundColor: "blue",
-    color: "white",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  text4: {
-    width: "100px",
-    backgroundColor: "wheat",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  avg: {
-    width: "100px",
-    backgroundColor: "black",
-    color: "white",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  var: {
-    width: "100px",
-    backgroundColor: "yellow",
-    color: "black",
-    textAlign: "center",
-    height: "60px",
-    lineHeight: "60px",
-    fontWeight: "bolder",
-  },
-  top: {
-    fontWeight: "bolder",
-    textAlign: "center",
-  },
-});
+import Table from "./Table";
+import { Button } from "@material-ui/core";
 function Home() {
-  const styles = useStyles();
-  const [data, setdata] = useState({
-    long1: "",
-    long2: "",
-    short1: "",
-    short2: "",
-    spread1: "",
-    spread2: "",
-    var1: "",
-    avg: "",
-    varianceArr: [],
-  });
-  //const [loading, setloading] = useState(false);
-  //const [rerender, setrerender] = useState(false);
-  const [state, setstate] = useState({ firm1: "", firm2: "" });
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setstate({ ...state, [name]: value });
-    const query =
-      name === "firm1"
-        ? `firm1=${value}&firm2=${state.firm2}`
-        : `firm1=${state.firm1}&firm2=${value}`;
-    axios.get(`${baseURL.url}/getdata?${query}`).then((res) => {
-      if (res && res.data) {
-        setdata(res.data);
-        const {
-          long1,
-          long2,
-          spread1,
-          spread2,
-          short1,
-          short2,
-          var1,
-          varianceArr,
-          avg,
-        } = res.data;
-        setdata({
-          long1,
-          long2,
-          spread1,
-          spread2,
-          short1,
-          short2,
-          var1,
-          varianceArr,
-          avg,
-        });
-      }
-    });
+  const [count, setcount] = useState([1]);
+  const changeCount = (n) => {
+    if (n === -1 && count.length > 1)
+      setcount(new Array(count.length + n).fill(1));
+    else if (n === 1) setcount(new Array(count.length + n).fill(1));
   };
-
   return (
     <>
-      <div className="w-full text-sm  p-5">
-        {0 ? (
-          <>
-            <Card style={{ maxWidth: "400px", margin: "0px auto" }}>
-              <img
-                alt="loader"
-                src="https://www.washingtonpost.com/graphics/2020/health/coronavirus-sars-cov-2-structure/coronavirus-big.gif?v=13"
-              ></img>
-            </Card>
-          </>
-        ) : (
-          <>
-            <div className="w-full my-3 overflow-x-auto ">
-              <div className="my-3 w-full flex">
-                <div>
-                  <Card
-                    className={`mx-2 ${styles.top}`}
-                    style={{
-                      width: 200,
-                    }}
-                  >
-                    Companies
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Long
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Short
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Spread
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Long
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Short
-                  </Card>
-                </div>
-                <div>
-                  <Card
-                    className={`mx-1 ${styles.top}`}
-                    style={{
-                      width: 100,
-                    }}
-                  >
-                    Spread
-                  </Card>
-                </div>
-
-                {data.avg !== "" && (
-                  <>
-                    {data.varianceArr.map((value, index) => {
-                      return (
-                        <div key={index}>
-                          <Card
-                            className={`mx-1 ${styles.top}`}
-                            style={{
-                              width: 100,
-                            }}
-                          >
-                            Week {index + 1}
-                          </Card>
-                        </div>
-                      );
-                    })}
-                    <div>
-                      <Card
-                        className={`mx-1 ${styles.top}`}
-                        style={{
-                          width: 100,
-                        }}
-                      >
-                        Average
-                      </Card>
-                    </div>
-                  </>
-                )}
-              </div>
-              <div className="flex">
-                <div>
-                  <Card className="mx-1">
-                    <TextField
-                      name="firm1"
-                      value={state.firm1}
-                      className={styles.textField}
-                      variant="outlined"
-                      onChange={onChange}
-                    />
-                  </Card>
-                </div>
-                <div>
-                  <Card className="mx-1">
-                    <TextField
-                      name="firm2"
-                      value={state.firm2}
-                      className={styles.textField}
-                      variant="outlined"
-                      onChange={onChange}
-                    />
-                  </Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text1}`}>{data.long1}</Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text2}`}>{data.short1}</Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text3}`}>{data.spread1}</Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text1}`}>{data.long2}</Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text2}`}>{data.short2}</Card>
-                </div>
-                <div>
-                  <Card className={`mx-1 ${styles.text3}`}>{data.spread2}</Card>
-                </div>
-
-                {data.avg !== "" && (
-                  <>
-                    {data.varianceArr.map((value, index) => {
-                      return (
-                        <div key={index}>
-                          <Card
-                            style={{ color: value < 0 ? "red" : "green" }}
-                            className={`mx-1 ${styles.text4}`}
-                          >
-                            {value}
-                          </Card>
-                        </div>
-                      );
-                    })}
-                    <div>
-                      <Card className={`mx-1 ${styles.avg}`}>{data.avg}</Card>
-                    </div>
-                  </>
-                )}
-              </div>
-            </div>
-          </>
-        )}
+      {count.map(() => (
+        <Table />
+      ))}
+      <div className="flex">
+        <div className="mx-2">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => changeCount(1)}
+            classname="mx-2"
+          >
+            Add
+          </Button>
+        </div>
+        <div className="mx-2">
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={() => changeCount(-1)}
+          >
+            Del
+          </Button>
+        </div>
       </div>
     </>
   );
---
+++ src/Pages/Table.js
@@ -0,0 +1,308 @@
+import React, { useState } from "react";
+import axios from "axios";
+//import moment from "moment";
+import { Card, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+const baseURL = { url: process.env.REACT_APP_BASE_URL };
+const useStyles = makeStyles({
+  textField: {
+    width: "100px",
+  },
+  text1: {
+    width: "100px",
+    backgroundColor: "red",
+    color: "white",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  text2: {
+    width: "100px",
+    backgroundColor: "green",
+    color: "white",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  text3: {
+    width: "100px",
+    backgroundColor: "blue",
+    color: "white",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  text4: {
+    width: "100px",
+    backgroundColor: "wheat",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  avg: {
+    width: "100px",
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  var: {
+    width: "100px",
+    backgroundColor: "yellow",
+    color: "black",
+    textAlign: "center",
+    height: "60px",
+    lineHeight: "60px",
+    fontWeight: "bolder",
+  },
+  top: {
+    fontWeight: "bolder",
+    textAlign: "center",
+  },
+});
+function Table() {
+  const styles = useStyles();
+  const [data, setdata] = useState({
+    long1: "",
+    long2: "",
+    short1: "",
+    short2: "",
+    spread1: "",
+    spread2: "",
+    var1: "",
+    avg: "",
+    varianceArr: [],
+  });
+  //const [loading, setloading] = useState(false);
+  //const [rerender, setrerender] = useState(false);
+  const [state, setstate] = useState({ firm1: "", firm2: "" });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setstate({ ...state, [name]: value });
+    const query =
+      name === "firm1"
+        ? `firm1=${value}&firm2=${state.firm2}`
+        : `firm1=${state.firm1}&firm2=${value}`;
+    axios.get(`${baseURL.url}/getdata?${query}`).then((res) => {
+      if (res && res.data) {
+        setdata(res.data);
+        const {
+          long1,
+          long2,
+          spread1,
+          spread2,
+          short1,
+          short2,
+          var1,
+          varianceArr,
+          avg,
+        } = res.data;
+        setdata({
+          long1,
+          long2,
+          spread1,
+          spread2,
+          short1,
+          short2,
+          var1,
+          varianceArr,
+          avg,
+        });
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="w-full text-sm ">
+        {0 ? (
+          <>
+            <Card style={{ maxWidth: "400px", margin: "0px auto" }}>
+              <img
+                alt="loader"
+                src="https://www.washingtonpost.com/graphics/2020/health/coronavirus-sars-cov-2-structure/coronavirus-big.gif?v=13"
+              ></img>
+            </Card>
+          </>
+        ) : (
+          <>
+            <div className="w-full my-1 overflow-x-auto ">
+              <div className="my-3 w-full flex">
+                <div>
+                  <Card
+                    className={`mx-2 ${styles.top}`}
+                    style={{
+                      width: 200,
+                    }}
+                  >
+                    Companies
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Long
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Short
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Spread
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Long
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Short
+                  </Card>
+                </div>
+                <div>
+                  <Card
+                    className={`mx-1 ${styles.top}`}
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    Spread
+                  </Card>
+                </div>
+
+                {data.avg !== "" && (
+                  <>
+                    {data.varianceArr.map((value, index) => {
+                      return (
+                        <div key={index}>
+                          <Card
+                            className={`mx-1 ${styles.top}`}
+                            style={{
+                              width: 100,
+                            }}
+                          >
+                            Week {index + 1}
+                          </Card>
+                        </div>
+                      );
+                    })}
+                    <div>
+                      <Card
+                        className={`mx-1 ${styles.top}`}
+                        style={{
+                          width: 100,
+                        }}
+                      >
+                        Average
+                      </Card>
+                    </div>
+                  </>
+                )}
+              </div>
+              <div className="flex">
+                <div>
+                  <Card className="mx-1">
+                    <TextField
+                      name="firm1"
+                      value={state.firm1}
+                      className={styles.textField}
+                      variant="outlined"
+                      onChange={onChange}
+                    />
+                  </Card>
+                </div>
+                <div>
+                  <Card className="mx-1">
+                    <TextField
+                      name="firm2"
+                      value={state.firm2}
+                      className={styles.textField}
+                      variant="outlined"
+                      onChange={onChange}
+                    />
+                  </Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text1}`}>{data.long1}</Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text2}`}>{data.short1}</Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text3}`}>{data.spread1}</Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text1}`}>{data.long2}</Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text2}`}>{data.short2}</Card>
+                </div>
+                <div>
+                  <Card className={`mx-1 ${styles.text3}`}>{data.spread2}</Card>
+                </div>
+
+                {data.avg !== "" && (
+                  <>
+                    {data.varianceArr.map((value, index) => {
+                      return (
+                        <div key={index}>
+                          <Card
+                            style={{ color: value < 0 ? "red" : "green" }}
+                            className={`mx-1 ${styles.text4}`}
+                          >
+                            {value}
+                          </Card>
+                        </div>
+                      );
+                    })}
+                    <div>
+                      <Card className={`mx-1 ${styles.avg}`}>{data.avg}</Card>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Table;
